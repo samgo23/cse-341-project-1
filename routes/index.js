567c8927d30e4c72632404eaf16acc1ae253bde8
samgo23 <gordsam18@gmail.com>
2023-04-29 13:58:13 -0600

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
+
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
